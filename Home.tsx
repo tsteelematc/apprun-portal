cb94ea29965d3145c3a62c20066bc09e79d6cad3
Tom Steele <tsteele@nationaldecisionsupport.com>
2019-01-17 12:15:05 -0600

    Using AppRun events...
    Specifically 'local' component events... With this.run() ! ! !

--- Home.tsx
+++ Home.tsx
@@ -6,11 +6,15 @@ export default class HomeComponent extends Component {
   view = (state) => {
     return <div>
       <h1>{state}</h1>
+      <button class="ms-Button ms-Button--primary">
+        <span class="ms-Button-label" onclick={(state) => this.run('Foo')}>Foo</span> 
+      </button>
     </div>
   }
 
   update = {
-    '#Home': state => state,
+    '#Home': state => state
+    ,'Foo': state => console.log('Bar')
   }
 }
 
