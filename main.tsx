e31d7e0257a836ec04f9e593fd8377ba4d8702c8
Tom Steele <tsteele@nationaldecisionsupport.com>
2019-01-17 11:06:22 -0600

    Fabric-JS kind of working...
    This is a bit of a downgrade from fabric-core...
    
    But I'm going to keep swimming for a bit...

--- main.tsx
+++ main.tsx
@@ -10,27 +10,20 @@ app.on('//', route => {
 })
 
 const view = state => <div className="container">
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">NDSC Enterprise Analytics</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="#Home">Home
-            <span className="sr-only">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#About">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#Contact">Contact</a>
-        </li>
-      </ul>
-    </div>
+  <nav className="ms-Pivot">
+    <ul className="ms-Pivot-links">
+      <li className="ms-Pivot-link is-selected">
+        <a className="nav-link" href="#Home">Home
+          <span className="sr-only">(current)</span>
+        </a>
+      </li>
+      <li className="ms-Pivot-link">
+        <a className="nav-link" href="#About">About</a>
+      </li>
+      <li className="ms-Pivot-link">
+        <a className="nav-link" href="#Contact">Contact</a>
+      </li>
+    </ul>
   </nav>
   <div className="container" id="my-app"></div>
 </div>
