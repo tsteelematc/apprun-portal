97fa7a9f2384e7e80d14e4fef1cdb6c6eb5465d3
Tom Steele <tsteele@nationaldecisionsupport.com>
2019-01-17 10:09:42 -0600

    Branded NDSC Enterprise Analytics...

--- main.tsx
+++ main.tsx
@@ -11,7 +11,7 @@ app.on('//', route => {
 
 const view = state => <div className="container">
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Project Name</a>
+    <a className="navbar-brand" href="#">NDSC Enterprise Analytics</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
       aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
