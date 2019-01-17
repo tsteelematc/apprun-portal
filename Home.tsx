48dc0d2081f0b7ccf67a4b8fe54792893aaf4d55
Tom Steele <tsteele@nationaldecisionsupport.com>
2019-01-17 12:20:43 -0600

    ms-Button working'ish...
    *className* over *class* dummy : - )

--- Home.tsx
+++ Home.tsx
@@ -6,15 +6,21 @@ export default class HomeComponent extends Component {
   view = (state) => {
     return <div>
       <h1>{state}</h1>
-      <button class="ms-Button ms-Button--primary">
-        <span class="ms-Button-label" onclick={(state) => this.run('Foo')}>Foo</span> 
+      <button className="ms-Button ms-Button--primary">
+        <span className="ms-Button-label" onclick={(state) => this.run('Foo')}>Foo</span> 
       </button>
-    </div>
+      &nbsp;
+      &nbsp;
+      <button className="ms-Button ms-Button">
+        <span className="ms-Button-label" onclick={(state) => this.run('Cat')}>Cat</span> 
+      </button>
+    </div>    
   }
 
   update = {
     '#Home': state => state
     ,'Foo': state => console.log('Bar')
+    ,'Cat': state => console.log('Dog')
   }
 }
 
